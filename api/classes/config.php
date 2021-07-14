bbaf1377e8c26e2a25e933a3d649a65c60ce1a5a
JoApp <7024431+uproid@users.noreply.github.com>
2021-07-14 14:42:33 +0200

    Create config.php

---
+++ api/classes/config.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Config used for the all basic options website
+ */
+
+/**
+ * database class used for access to Database
+ *
+ * @author Farhad Ziaee
+ */
+class database {
+    public $db_ip = "localhost";
+    public $db_name = "library";
+    public $db_username = "root";
+    public $db_password = "";
+}
